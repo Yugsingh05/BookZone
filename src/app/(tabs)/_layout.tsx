09bb6adbe9141f6bd75d78f8ae96bd76cbd4dcea
Yugsingh05 <yugsingh7622@gmail.com>
2025-04-23 14:41:25 +0530

    feat : Added the loadinng incator

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,10 +1,15 @@
 import { Redirect, Tabs } from "expo-router"
 import {Ionicons} from "@expo/vector-icons"
 import { useAuth } from "@clerk/clerk-expo"
+import { ActivityIndicator } from "react-native";
 
 export default function TabsLayout() {
 
-     const { isSignedIn } = useAuth()
+     const { isSignedIn , isLoaded } = useAuth();
+
+     if(isLoaded){
+      return <ActivityIndicator/>
+     }
     
       if (!isSignedIn) {
         return <Redirect href={'/sign-in'} />
