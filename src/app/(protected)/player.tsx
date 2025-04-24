94ee8e5a366be2b1761932282505f98a00a954fd
Yugsingh05 <yugsingh7622@gmail.com>
2025-04-24 08:20:22 +0530

    feat : design part of player screen is done

--- src/app/(protected)/player.tsx
+++ src/app/(protected)/player.tsx
@@ -5,13 +5,18 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import DummyBooks from "@/lib/dummyBooks";
 import { Ionicons } from "@expo/vector-icons";
 import PlaybackBar from "@/components/PlayBackBar";
+import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
 
 export default function Player() {
   const book = DummyBooks[0];
+
+  const player = useAudioPlayer(book.audio_url);
+  const playerStatus = useAudioPlayerStatus(player);
+
   return (
-    <SafeAreaView className="flex-1 bg-gray-900 p-4 py-10 gap-4">
+    <SafeAreaView className="flex-1  p-4 py-10 gap-4">
       <Pressable
-        className="absolute top-16 left-4 bg-gray-800 rounded-full p-2"
+        className="absolute top-4 left-4 bg-gray-800 rounded-full p-2"
         onPress={() => router.back()}
       >
         <Entypo name="chevron-down" size={24} color={"white"} />
@@ -19,7 +24,7 @@ export default function Player() {
 
       <Image
         source={{ uri: book.thumbnail_url }}
-        className="w-[95%] aspect-square rounded-[30px] self-center"
+        className="w-[95%] aspect-square rounded-[30px] self-center mt-10"
       />
 
       <View className="gap-8 flex-1 justify-end">
@@ -27,12 +32,17 @@ export default function Player() {
           {book.title}
         </Text>
 
-        <PlaybackBar value={0.5} />
+        <PlaybackBar currentTime={playerStatus.currentTime} duration={playerStatus.duration}/>
 
         <View className="flex-row items-center justify-between mt-8">
           <Ionicons name="play-skip-back" size={24} color="white" />
           <Ionicons name="play-back" size={24} color="white" />
-          <Ionicons name="play" size={50} color="white" />
+          <Ionicons
+            onPress={() =>playerStatus.playing ? player.pause() : player.play()}
+            name={playerStatus.playing ? "pause" : "play"}
+            size={50}
+            color="white"
+          />
           <Ionicons name="play-forward" size={24} color="white" />
           <Ionicons name="play-skip-forward" size={24} color="white" />
         </View>
--- src/components/PlayBackBar.tsx
+++ src/components/PlayBackBar.tsx
@@ -1,16 +1,38 @@
-import { View } from 'react-native';
+import { Text, View } from "react-native";
 
-export default function PlaybackBar({ value }: { value: number }) {
+type PlayBackBarProps = {
+  currentTime: number;
+  duration: number;
+};
+
+export default function PlaybackBar({
+  currentTime,
+  duration,
+}: PlayBackBarProps) {
+  const progress = currentTime / duration;
+
+  const formatTime = (time : number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  }
   return (
-    <View className='w-full bg-slate-900 h-2 rounded-full justify-center'>
-      <View
-        className='bg-orange-400 h-full rounded-full'
-        style={{ width: `${value * 100}%` }}
-      />
-      <View
-        className='absolute w-3 h-3 -translate-x-1/2 rounded-full bg-orange-400'
-        style={{ left: `${value * 100}%` }}
-      />
+    <View className=" gap-4">
+      <View className="w-full bg-slate-900 h-2 rounded-full justify-center">
+        <View
+          className="bg-orange-400 h-full rounded-full"
+          style={{ width: `${progress * 100}%` }}
+        />
+        <View
+          className="absolute w-3 h-3 -translate-x-1/2 rounded-full bg-orange-400"
+          style={{ left: `${progress * 100}%` }}
+        />
+      </View>
+
+      <View className="flex justify-between align-center flex-row ">
+        <Text className="text-white">{formatTime(currentTime)}</Text>
+        <Text className="text-white">{formatTime(duration)}</Text>
+      </View>
     </View>
   );
 }
